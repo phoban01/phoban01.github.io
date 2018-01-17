2ca6005516d00f2fc3fb1a6fafebc633d307a88f
TaisceCheol <piaras.hoban@itma.ie>
2018-01-17 11:14:10 +0000

    add css grid polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp 		= require('gulp'),
 	livereload 	= require('gulp-livereload'),
 	sass		= require('gulp-sass'),
+	concat		= require('gulp-concat'),
+	uglify		= require('gulp-uglify'),
 	minify		= require('gulp-compress');
 
-livereload({ start: true,options:{port:8080} });
+var bower_path = 'bower_components/'
+var js_srcs = [
+	bower_path + 'css-grid-polyfill-binaries/css-polyfills.min.js',
+	'js/main.js'
+]
+
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
@@ -22,10 +29,18 @@ gulp.task('html', function() {
     .pipe(livereload());
 });
 
+gulp.task('js',function() {
+	return gulp.src(js_srcs)
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+	.pipe(gulp.dest('js/'))
+})
+
 gulp.task('watch',function() {
+	livereload({ start: true,options:{port:8080} });	
 	gulp.watch('scss/**/*.scss',['sass'])
 	gulp.watch('*.html',['html'])
-	gulp.watch('*.js',['html'])
+	gulp.watch('*.js',['js','html'])
 
 })
 
--- js/app.js
+++
@@ -1,8 +0,0 @@
-var images = $('div > h2 + img');
-
-for (var i=0;i < images.length;i++) {
-	var r = Math.random();
-	if (r > 0.3) {
-		$(images[i]).removeClass('hide-svg')
-	}
-}
