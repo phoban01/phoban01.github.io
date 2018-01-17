3ceaa1ee138585679ed0f8af1790acc6ff7b8af6
TaisceCheol <piaras.hoban@itma.ie>
2018-01-17 12:15:44 +0000

    added viewport hacks

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp 		= require('gulp'),
 var bower_path = 'bower_components/'
 var js_srcs = [
 	bower_path + 'viewport-units-buggyfill/viewport-units-buggyfill.js',
+	bower_path + 'viewport-units-buggyfill/viewport-units-buggyfill.hacks.js',
 	bower_path + 'css-grid-polyfill-binaries/css-polyfills.min.js',
 	bower_path + 'flexibility/flexibility.js',
 	bower_path + 'jquery/dist/jquery.min.js',
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 var $elements = $('.flex-center');
 
 $('.flex-center').each(function(el) {
-	console.log($(this)[0])
 	flexibility($(this)[0]);
 });
 
-window.viewportUnitsBuggyfill.init();
+viewportUnitsBuggyfill.init({hacks: window.viewportUnitsBuggyfillHacks});
